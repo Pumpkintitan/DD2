d4fdb6b762a63ed61d7b7bd80a69f1ba7a616148
Pumpkintitan <enbl2915@colorado.edu>
2022-03-01 10:58:23 -0700

    Fixed moving and network graphing

--- Brain.js
+++ Brain.js
@@ -1,5 +1,105 @@
 class Brain {
-    constructor(decoded, hidden) {
-        
+    constructor(decoded, hidden, sb) {
+        if (sb) {
+            console.log(decoded)
+            let pnodes = [
+                { id: 1, group: 'inp', label: 'Age' }, //Age
+                { id: 2, group: 'inp', label: 'Rnd' }, //Random Input
+                { id: 3, group: 'inp', label: 'Pfd' }, //Pop density forwards
+                { id: 4, group: 'inp', label: 'Plr' }, //Pop density L/R
+                { id: 5, group: 'inp', label: 'LPf' }, //Pop density long range forwards
+                { id: 6, group: 'inp', label: 'LMy' }, //Last move y                
+                { id: 7, group: 'inp', label: 'LMx' }, //Last move x
+                { id: 8, group: 'inp', label: 'BDy' }, //Border distance y
+                { id: 9, group: 'inp', label: 'BDx' }, //Border distance x
+                { id: 10, group: 'inp', label: 'Lx' }, //x location
+                { id: 11, group: 'inp', label: 'Ly' }, //y location
+                { id: 12, group: 'inp', label: 'Bd' }, //Closest border
+                { id: 13, group: 'inp', label: 'Gen' }, //Genetic similarity of forward neighbor
+                { id: 14, group: 'inp', label: 'PLd' }, //Distance to closest plant
+                { id: 15, group: 'inp', label: 'Eng' }, //Energy
+                { id: 16, group: 'inp', label: 'DEg' }, //Change in energy from last turn
+                { id: 17, group: 'inp', label: 'Pdf' }, //Plant density forwards
+                { id: 18, group: 'inp', label: 'Osc' }, //Oscillator
+                { id: 19, group: 'inp', label: 'PiD' }, //Plants within 5 moves
+                { id: 20, group: 'inp', label: 'PDy' }, //Plant distance y
+                { id: 21, group: 'inp', label: 'PDx' }, //Plant distance x
+                { id: 22, group: 'hid', label: 'N0' }, //Hidden node 0
+                { id: 23, group: 'hid', label: 'N1' }, //Hidden node 1
+                { id: 24, group: 'hid', label: 'N2' }, //Hidden node 2
+                { id: 25, group: 'hid', label: 'N3' }, //Hidden node 3
+                { id: 26, group: 'hid', label: 'N4' }, //Hidden node 4
+                { id: 27, group: 'hid', label: 'N5' }, //Hidden node 5
+                { id: 28, group: 'hid', label: 'N6' }, //Hidden node 6
+                { id: 29, group: 'hid', label: 'N7' }, //Hidden node 7
+                { id: 30, group: 'hid', label: 'N8' }, //Hidden node 8
+                { id: 31, group: 'hid', label: 'N9' }, //Hidden node 9
+                { id: 32, group: 'out', label: 'OSC' }, //Set oscillator period
+                { id: 33, group: 'out', label: 'Mfd' }, //Move forwards
+                { id: 34, group: 'out', label: 'Mrn' }, //Move random
+                { id: 35, group: 'out', label: 'Mrv' }, //Move reverse
+                { id: 36, group: 'out', label: 'Mx' }, //Move x
+                { id: 37, group: 'out', label: 'My' }, //Move y
+                { id: 38, group: 'out', label: 'Mrl' }, //Move left/right
+                { id: 39, group: 'out', label: 'Plt' }, //Plant forwards
+                { id: 40, group: 'out', label: 'NON' }, //Do nothing
+                { id: 41, group: 'out', label: 'Spn' }, //Spawn offspring forwards
+            ]
+
+            var nodes = new vis.DataSet([]);
+            var ee = []
+            for (let i = 0; i < decoded.length; i++) {
+                let ton = 0
+                let from = 0
+                if (decoded[i][0] == 1) {
+                    from = (decoded[i][1] % hidden) + 21
+                } else {
+                    from = (decoded[i][1] % 21)
+                }
+                if (decoded[i][2] == 1) {
+                    ton = (decoded[i][3] % hidden) + 21
+                } else {
+                    ton = (decoded[i][3] % 10) + 31
+                }
+                nodes.update(pnodes[ton])
+                nodes.update(pnodes[from])
+                if (decoded[i][4] < 0) {
+                    ee.push(({ from: from + 1, to: ton + 1, color: "red" }))
+                }
+                if (decoded[i][4] > 0) {
+                    ee.push(({ from: from + 1, to: ton + 1, color: "green" }))
+                }
+            }
+            var edges = new vis.DataSet([]);
+            for (let i = 0; i < ee.length; i++) {
+                console.log(ee[i])
+                edges.update(ee[i])
+            }
+            // create a network
+            var container = document.getElementById('status');
+
+            // provide the data in the vis format
+            var data = {
+                nodes: nodes,
+                edges: edges
+            };
+            var options = {
+                groups: {
+                    inp: { color: { background: '#77aaff' } },
+                    out: { color: { background: '#ff8888' } },
+                    hid: { color: { background: '#aaaaaa' } }
+                },
+                edges: {
+                    arrows: {
+                        to: {
+                            enabled: true,
+                        }
+                    }
+                }
+            };
+
+            // initialize your network!
+            var network = new vis.Network(container, data, options);
+        }
     }
 }
--- Dot.js
+++ Dot.js
@@ -1,12 +1,12 @@
 class Dot {
-    constructor(glength, sx, sy, h) {
+    constructor(glength, sx, sy, h, sb) {
         this.genome = new Genome()
         this.genome.generate(glength)
         this.pos = createVector(sx, sy)
-        this.brain = new Brain(this.genome.decode(), h)
+        this.brain = new Brain(this.genome.decode(), h, sb)
         let s = this.genome.s.replace(/\s/g, "")
-        let l = Math.round(s.length/3)
-        let l2 = Math.round(2*s.length/3)
+        let l = Math.round(s.length / 3)
+        let l2 = Math.round(2 * s.length / 3)
         this.r = (parseInt(s.substring(0, l), 16) % 150) + 50
         this.g = (parseInt(s.substring(l, l2), 16) % 150) + 50
         this.b = (parseInt(s.substring(l2, s.length), 16) % 150) + 50
@@ -17,30 +17,7 @@ class Dot {
         ellipse(this.pos.x * 6 + 3, this.pos.y * 6 + 3, 6)
     }
 
-    move(mapp, dir) {
-
-        switch(dir) {
-            case 0: 
-                if (this.pos.x != 99 && mapp[this.pos.y][this.pos.x + 1] == 0 ) {
-                    this.pos.x += 1
-                }
-                break;
-            case 1: 
-                if (this.pos.x != 0 && mapp[this.pos.y][this.pos.x - 1] == 0) {
-                    this.pos.x -= 1
-                }
-                break;
-            case 2: 
-                if (this.pos.y != 99 && mapp[this.pos.y + 1][this.pos.x] == 0) {
-                    // console.log(mapp[this.pos.y + 1][this.pos.x])
-                    this.pos.y += 1
-                }
-                break;
-            case 3: 
-                if (this.pos.y != 0 && mapp[this.pos.y - 1][this.pos.x] == 0) {
-                    this.pos.y -= 1
-                }
-                break;
-        }
+    move() {
+        return Math.floor(Math.random() * 4)
     }
 }
